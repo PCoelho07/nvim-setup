16d6a6c8f881ef1bec1140def73acf490c8518a6
Pedro Coelho <pdrbt@outlook.com>
2024-11-25 08:43:39 -0300

    add search and replace for a line

--- lua/pcoelho/remap.lua
+++ lua/pcoelho/remap.lua
@@ -17,6 +17,7 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<leader>sl", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 vim.keymap.set("n", "<leader>zig", "<cmd>LspRestart<cr>")
 vim.keymap.set("n", "<leader>f", function()
