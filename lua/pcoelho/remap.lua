127d41b4ddb4ffd6ea9d289403bc180d6945281d
Pedro Coelho <pdrbt@outlook.com>
2025-04-18 17:42:56 -0300

    add <leader>e to show the line error, and fix the auto identation for php files

--- lua/pcoelho/remap.lua
+++ lua/pcoelho/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
 
 -- this is for scrool the screen and keep my cursor at the middle
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
